34db0c0623216f5ecff58ffa5b8d10f5ace213cb
brittanypugh <brittany.honesty@gmail.com>
2014-09-17 16:21:17 -0400

    Deleted unnecessary files

--- memoryb.js
+++
@@ -1,6 +0,0 @@
-// Concentration Memory Game - Body Script
-// copyright Stephen Chapman, 5th August 2005
-// you may copy this script provided that you retain the copyright notice
-
-document.write('<div align="center"><table cellpadding="0" cellspacing="0" border="0">');for (var a = 0; a <= 5; a++) {document.write('<tr>');for (var b = 0; b <= 5; b++) {document.write('<td align="center" class="blk" id="t'+((6*a)+b)+'"></td>');}document.write('<\/tr>');}document.write('<\/table><form name="mem"><input type="button" name="cnt" value="0:00" onclick="begin()" \/><\/form><\/div>');
-                    
